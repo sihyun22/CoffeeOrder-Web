dda549543829fefc101210b417ec6b7e5cc099cf
sihyunpark <sihyun22>
2024-12-17 17:43:24 +0900

    second commit

--- main.js
+++ main.js
@@ -14,84 +14,112 @@ closeCart.onclick = () => {
 
 
 // 장바구니 js //
-if(document.readyState == 'loading'){
+if (document.readyState == 'loading') {
   document.addEventListener('DOMContentLoaded', ready)
-}else{
+} else {
   ready();
 }
 
+// 메인 홈페이지 이미지
+let vedioArea = document.getElementById('videoArea');
+// 상단 메뉴 Tab 했을 때 발생되는 클릭 함수
+
+// 베스트 메뉴
+let bestTab = document.getElementById('bestTap');
+bestTab.onclick = () =>{
+  // active2 클래스 추가.
+  bestMenuArea.classList.add('active2');
+  bestMenuArea.classList.remove('active'); 
+  coffeeMenuArea.classList.remove('active');
+  vedioArea.classList.add('active');
+  if(teaTab.onclick===true){
+    bestMenuArea.classList.add('active2');
+  }
+  teaMenuArea.classList.remove('active3')
+}
+let bestMenuArea = document.getElementById('best-menu-area');
+let coffeeTab = document.getElementById('coffeeTab');
+let coffeeMenuArea = document.getElementById('coffee-menu-area');
+coffeeTab.onclick = () =>{
+  vedioArea.classList.add('active');
+  // 커피 메뉴 클래스 active(보이게) 추가
+  coffeeMenuArea.classList.add('active');
+  // 베스트 메뉴 클래스 active(안보이게)
+  bestMenuArea.classList.add('active');
+  // 베스트 메뉴 클래스 active(보이게)
+  bestMenuArea.classList.remove('active2');
+  // bestMenuArea.classList.remove('active');
+  teaMenuArea.classList.remove('active3');
+}
+let teaMenuArea = document.getElementById('tea-menu-area');
+let teaTab = document.getElementById('teaTab');
+teaTab.onclick = () =>{
+  vedioArea.classList.add('active');
+  // 티 메뉴 클래스 보이게 
+  teaMenuArea.classList.add('active3');
+  // 커피 메뉴 클래스 active(보이게) 추가
+  coffeeMenuArea.classList.remove('active');
+  // 베스트 메뉴 클래스 active(안보이게)
+  bestMenuArea.classList.add('active');
+  // 베스트 메뉴 클래스 active(보이게)
+  bestMenuArea.classList.remove('active2');
+  // bestMenuArea.classList.remove('active');
+}
 // 함수 
 
-function ready(){
+function ready() {
   // 주문내역 제거
   let removeCartButtons = document.getElementsByClassName('cart-remove');
-  for(let i = 0 ; i<removeCartButtons.length; i++){
+  for (let i = 0; i < removeCartButtons.length; i++) {
     let button = removeCartButtons[i];
     button.addEventListener('click', removeCartItem)
   }
   // 수량 변경
   let quantityInputs = document.getElementsByClassName('cart-quantity');
-  for(let i = 0 ; i<quantityInputs.length; i++){
+  for (let i = 0; i < quantityInputs.length; i++) {
     let input = quantityInputs[i];
     input.addEventListener('change', quantityChanged);
   }
 }
-function removeCartItem(event){
+function removeCartItem(event) {
   let buttonClicked = event.target;
   buttonClicked.parentElement.remove();
   updatetotal();
 }
 // 수량 변경 함수
-function quantityChanged(event){
+function quantityChanged(event) {
   let input = event.target;
-  if(isNaN(input.value) || input.value <=0){
+  if (isNaN(input.value) || input.value <= 0) {
     input.value = 1;
   }
   updatetotal();
 }
 
 // Update Total
-function updatetotal(){
+function updatetotal() {
   let cartContent = document.getElementsByClassName('cart-content');
   let cartBoxes = document.getElementsByClassName('cart-box');
   let total = 0;
-  for(let i = 0; i<cartBoxes.length; i++){
+  for (let i = 0; i < cartBoxes.length; i++) {
     let cartBox = cartBoxes[i];
     let priceElement = cartBox.getElementsByClassName('cart-price')[0];
     let quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
-    let price = parseFloat(priceElement.innerText.replace(/,/g,""))
-    console.log(price);
+    let price = parseInt(priceElement.innerText.replace(/[^0-9]/g, ""), 10);
     let quantity = quantityElement.value;
-    function totalComma(total){
-      return total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-    }
-    total = totalComma(total + (price * quantity));
-    
-    document.getElementsByClassName('total-price')[0].innerText = total + '원';
+    total = total + price * quantity;
   }
+  document.getElementsByClassName('total-price')[0].innerText = (total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')) + ' 원';
+
 }
-// 장바구니 아이템 삭제 //
-
-// // 장바구니에 추가된 메뉴 박스 //
-// let cartBox = document.querySelector('.cart-box');
-// // 장바구니 삭제 아이콘 가져오기 //
-// let cartRemove = document.querySelector('.cart-remove');
-// cartRemove.onclick = () => {
-//   cartBox.style.display = "none";
-// }
-
-// // 장바구니 에 메뉴 추가하기 //
-
-
 // 메뉴의 + 아이콘 가져오기 //
 
 let addCart = document.getElementsByClassName("add-cart");
-for(let i = 0; i<addCart.length; i++){
+for (let i = 0; i < addCart.length; i++) {
   let button = addCart[i];
   button.addEventListener("click", addCartClicked);
 }
 
-function addCartClicked(event){
+function addCartClicked(event) {
   let button = event.target;
   let shopProducts = button.parentElement;
   let title = shopProducts.getElementsByClassName('product-title')[0].innerText;
@@ -99,21 +127,22 @@ function addCartClicked(event){
   let img = shopProducts.getElementsByClassName('best-menu-img')[0].src;
   addProductToCart(title, price, img);
   updatetotal();
-  console.log(title, price, img);
+  // console.log(title, price, img);
 }
 
-function addProductToCart(title, price, img){
+function addProductToCart(title, price, img) {
   let cartShopBox = document.createElement('div');
   cartShopBox.classList.add('cart-box');
   let cartItems = document.getElementsByClassName('cart-content')[0];
   let cartItemsNames = cartItems.getElementsByClassName('cart-product-title');
-  for (let i = 0; i < cartItemsNames.length; i++){
-    if(cartItemsNames[i].innerText == title){
+  for (let i = 0; i < cartItemsNames.length; i++) {
+    if (cartItemsNames[i].innerText == title) {
       alert('이미 장바구니에 추가 된 상품입니다.')
       return;
     }
 
   }
+
   let cartBoxContent = `
   <img src="${img}" alt="" class="cart-img">
  <div class="detail-box">
@@ -126,10 +155,9 @@ function addProductToCart(title, price, img){
  <i class='bx bxs-x-circle cart-remove'></i>
 `;
 
-    cartShopBox.innerHTML = cartBoxContent;
+  cartShopBox.innerHTML = cartBoxContent;
   cartItems.append(cartShopBox);
   cartShopBox.getElementsByClassName('cart-remove')[0].addEventListener('click', removeCartItem);
   cartShopBox.getElementsByClassName('cart-quantity')[0].addEventListener('change', quantityChanged);
+
 }
-
-
